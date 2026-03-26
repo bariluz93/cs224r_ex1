81dab102e432cf33943415830356e0edbca61bb4
bareluz <bar.iluz@mail.huji.ac.il>
2026-03-26 18:40:47 +0200

    implemented pytorch_utis and replay_buffer

--- cs224r/infrastructure/pytorch_util.py
+++ cs224r/infrastructure/pytorch_util.py
@@ -54,10 +54,43 @@ def build_mlp(
     if isinstance(output_activation, str):
         output_activation = _str_to_activation[output_activation]
 
-    # TODO: return a MLP. This should be an instance of nn.Module
+    # return a MLP. This should be an instance of nn.Module
     # Note: nn.Sequential is an instance of nn.Module.
+    # 2. Create a list and append layers in order
+
+    layers = []
+
+    # Add the first hidden layer (input to first hidden)
+
+    layers.append(nn.Linear(input_size, size))
+
+    layers.append(activation) # Activation function
+
     
-    raise NotImplementedError
+
+    # Add subsequent hidden layers dynamically
+
+    for i in range(len(n_layers) - 1):
+
+        layers.append(nn.Linear(size, size))
+
+        layers.append(activation)
+
+    
+
+    # Add the output layer (last hidden to output)
+
+    layers.append(nn.Linear(size, output_size))
+
+
+    layers.append(output_activation) 
+
+    
+
+    # 3. Create the MLP model using nn.Sequential and the * operator
+
+    return nn.Sequential(*layers)
+
 
 def init_gpu(use_gpu=True, gpu_id=0):
     global device
--- cs224r/infrastructure/replay_buffer.py
+++ cs224r/infrastructure/replay_buffer.py
@@ -120,16 +120,21 @@ class ReplayBuffer():
                 == self.terminals.shape[0]
         )
 
-        ## TODO return batch_size number of random entries\
+        ## return batch_size number of random entries\
         ## from each of the 5 component arrays above
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array
         ## (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
         ## Note that rews, next_obs, and terminals are not used for BC
-
-        raise NotImplementedError
-    
+        indices = np.random.permutation(self.obs.shape[0])[:batch_size]
+        return (
+            self.obs[indices],
+            self.acs[indices],
+            self.rews[indices],
+            self.next_obs[indices],
+            self.terminals[indices],
+        )
     def sample_recent_data(self, batch_size=1):
         """
         Samples a batch of the most recent transitions transitions
